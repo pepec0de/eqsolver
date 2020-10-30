68cbc977ae26cf7d23caf18eb52645071a694510
Pepe González <pepeglezabad@gmail.com>
2020-10-30 20:47:42 +0100

    Implementing stringutils class

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,15 @@
 #include <vector>
 
 #include "../include/equtils.h"
+#include "../include/stringutils.h"
 
 using namespace std;
-
-vector<string> splitString(string str, char deliminator);
-string getSubstring(string str, unsigned int start, unsigned int end);
-string getSubstring(string str, int fromEnd);
-string toStr(char value);
 
 float getEqGrade(vector<string> eqArr);
 void solveEq(vector<string> eqArr);
 
 equtils eqUtils;
+stringutils strUtils;
 
 int main() {
     cout << "Ecuación a resolver: ";
@@ -44,58 +41,11 @@ int main() {
 	}
 	
 	// Dividimos la string en ' ' para recoger los elementos y guardarlos en un vector
-	vector<string> eqArr = splitString(eqStr, ' ');
+	vector<string> eqArr = strUtils.splitString(eqStr, ' ');
 	cout << "Su ecuación es de nivel: "	<< getEqGrade(eqArr) << endl;
 	solveEq(eqArr);
 }
 
-/// STRING FUNCTIONS
-// Func to split a string with a given char
-vector<string> splitString(string str, char deliminator) {
-	vector<string> strVct;
-	unsigned int relStart = 0;
-	for (unsigned int i = 0; i < str.size(); i++) {
-		if (str[i] == deliminator) {
-			// Hacemos el substring y despues lo añadimos al vector str
-			strVct.push_back(getSubstring(str, relStart, i));
-			relStart = i+1;
-			// Si el delimitador esta en el ultimo caracter sacamos el vector
-			if (relStart >= str.size()) {
-				return strVct;
-			}
-		}
-	}
-	strVct.push_back(getSubstring(str, relStart, str.size()));
-	return strVct;
-}
-
-// Func to do substring having an interval
-string getSubstring(string str, unsigned int start, unsigned int end) {
-	string result = "";
-	for (unsigned int i = start; i < end; i++) {
-        result += str[i];
-	}
-	return result;
-}
-
-// Func to remove last characters of a string (has to be a negative number)
-string getSubstring(string str, int fromEnd) {
-	string result = "";
-	if (fromEnd < 0) {
-		for (int i = 0; i < (fromEnd + str.size()); i++) {
-			result.push_back(str[i]);
-		}
-	}
-	return result;
-}
-
-string toStr(char value) {
-	// Una string es un array de chars por tanto:
-	string str;
-	str.push_back(value);
-	return str;
-}
-/// EQUATION FUNCTIONS
 float getEqGrade(vector<string> eqArr) {
 	float grade = 0;
 	for (unsigned int i = 0; i < eqArr.size(); i++) {
@@ -131,14 +81,14 @@ void solveEq(vector<string> eqArr) {
 		vector<float> polyVar, polyVal;
 		for (unsigned int i = 0; i < poly1.size(); i++) {
 			if (eqUtils.hasVar(poly1[i])) {
-				polyVar.push_back(stof(getSubstring(poly1[i], -1)));
+				polyVar.push_back(stof(strUtils.getSubstring(poly1[i], -1)));
 			} else {
 				polyVal.push_back(stof(poly1[i])*(-1));
 			}
 		}
 		for (unsigned int i = 0; i < poly2.size(); i++) {
 			if (eqUtils.hasVar(poly2[i])) {
-				polyVar.push_back(stof(getSubstring(poly1[i], -2))*(-1));
+				polyVar.push_back(stof(strUtils.getSubstring(poly1[i], -2))*(-1));
 			} else {
 				polyVal.push_back(stof(poly2[i]));
 			}
---
+++ src/stringutils.cpp
@@ -0,0 +1,48 @@
+#include "../include/stringutils.h"
+
+// Func to split a string with a given char
+vector<string> stringutils::splitString(string str, char deliminator) {
+	vector<string> strVct;
+	unsigned int relStart = 0;
+	for (unsigned int i = 0; i < str.size(); i++) {
+		if (str[i] == deliminator) {
+			// Hacemos el substring y despues lo añadimos al vector str
+			strVct.push_back(getSubstring(str, relStart, i));
+			relStart = i+1;
+			// Si el delimitador esta en el ultimo caracter sacamos el vector
+			if (relStart >= str.size()) {
+				return strVct;
+			}
+		}
+	}
+	strVct.push_back(getSubstring(str, relStart, str.size()));
+	return strVct;
+}
+
+// Func to do substring having an interval
+string stringutils::getSubstring(string str, unsigned int start, unsigned int end) {
+	string result = "";
+	for (unsigned int i = start; i < end; i++) {
+        result += str[i];
+	}
+	return result;
+}
+
+// Func to remove last characters of a string (has to be a negative number)
+string stringutils::getSubstring(string str, int fromEnd) {
+	string result = "";
+	if (fromEnd < 0) {
+		for (int i = 0; i < (fromEnd + str.size()); i++) {
+			result.push_back(str[i]);
+		}
+	}
+	return result;
+}
+
+string stringutils::toStr(char value) {
+	// Una string es un array de chars por tanto:
+	string str;
+    str.push_back(value);
+    return str;
+}
+
